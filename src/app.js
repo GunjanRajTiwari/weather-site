6e3614ce9ab80e066642c64ab299f0be1fd1f970
Gunjan Raj Tiwari <im.gunjan1@gmail.com>
2021-07-23 18:54:37 +0545

    Minor Updates

--- src/app.js
+++ src/app.js
@@ -1,100 +1,113 @@
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const path = require("path");
+const express = require("express");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode");
+const forecast = require("./utils/forecast");
 
-const app = express()
-const port = process.env.PORT || 8000
+const app = express();
+const port = process.env.PORT || 8000;
 
 // Define paths for Express Config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const partialsPath = path.join(__dirname, '../partials')
+const publicDirectoryPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../partials");
 
 // Setup Handlebar engine and view location
-app.set('view engine', 'hbs');
-hbs.registerPartials(partialsPath)
+app.set("view engine", "hbs");
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-    res.render('index', {
-        title: 'Weather App',
-        name: 'Gunjan Raj Tiwari'
-    })
-})
+app.get("", (req, res) => {
+	res.render("index", {
+		title: "Weather Saga",
+		name: "Gunjan Raj Tiwari",
+	});
+});
 
-app.get('/about', (req, res) => {
-    res.render('about', {
-        title: 'About Me',
-        name: 'Gunjan Raj Tiwari'
-    })
-})
+app.get("/about", (req, res) => {
+	res.render("about", {
+		title: "About Me",
+		name: "Gunjan Raj Tiwari",
+	});
+});
 
-app.get('/help', (req, res) => {
-    res.render('help', {
-        title: 'Help Message',
-        message: 'This is a help message.',
-        name: 'Gunjan Raj Tiwari'
-    })
-})
+app.get("/help", (req, res) => {
+	res.render("help", {
+		title: "Help Message",
+		message: "This is a help message.",
+		name: "Gunjan Raj Tiwari",
+	});
+});
 
-app.get('/weather', (req, res) => {
-    if (!req.query.address){
-        return res.send({
-            error: 'Address must be provided.'
-        })
-    }
-    
-    geocode(req.query.address,(error, {latitude, longitude, location} = {}) => {
-        if (error){
-            return res.send({ error })
-        }
-    
-        forecast(latitude, longitude, (error, { type,
-            description,
-            temp,
-            minTemp,
-            maxTemp,
-            pressure,
-            humidity,
-            clouds,
-            dew,}) => {
-            if (error){
-                return res.send({ error })
-            }
-            return res.send({
-                location,
-                type,
-                description,
-                temp,
-                minTemp,
-                maxTemp,
-                pressure,
-                humidity,
-                clouds,
-                dew,
-            })
-        })
-    }) 
-})
+app.get("/weather", (req, res) => {
+	if (!req.query.address) {
+		return res.send({
+			error: "Address must be provided.",
+		});
+	}
 
-app.get('/help/*', (req,res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Gunjan Raj Tiwari',
-        message: 'Help article not found.'
-    })
-})
+	geocode(
+		req.query.address,
+		(error, { latitude, longitude, location } = {}) => {
+			if (error) {
+				return res.send({ error });
+			}
 
-app.get('*', (req,res) => {
-    res.render('404', {
-        title: '404',
-        name: 'Gunjan Raj Tiwari',
-        message: 'Page not found.'
-    })})
+			forecast(
+				latitude,
+				longitude,
+				(
+					error,
+					{
+						type,
+						description,
+						temp,
+						minTemp,
+						maxTemp,
+						pressure,
+						humidity,
+						clouds,
+						dew,
+					}
+				) => {
+					if (error) {
+						return res.send({ error });
+					}
+					return res.send({
+						location,
+						type,
+						description,
+						temp,
+						minTemp,
+						maxTemp,
+						pressure,
+						humidity,
+						clouds,
+						dew,
+					});
+				}
+			);
+		}
+	);
+});
+
+app.get("/help/*", (req, res) => {
+	res.render("404", {
+		title: "404",
+		name: "Gunjan Raj Tiwari",
+		message: "For any help you can mail me at: roarout20@gmail.com",
+	});
+});
+
+app.get("*", (req, res) => {
+	res.render("404", {
+		title: "404",
+		name: "Gunjan Raj Tiwari",
+		message: "Page not found.",
+	});
+});
 
 app.listen(port, () => {
-    console.log('Server is up on port '+port)
-})
+	console.log("Server is up on port " + port);
+});
